9f88c0148b45555372bc2c68622688fa100f8698
SaschaLF <100208823+SaschaLF@users.noreply.github.com>
2022-07-22 10:31:47 +0200

    Add files via upload

--- gro_e_digitale_Anzeige/gro_e_digitale_Anzeige.ino
+++ gro_e_digitale_Anzeige/gro_e_digitale_Anzeige.ino
@@ -7,7 +7,8 @@ const int ziffer2= 11;
 const int ziffer3= 12;
 const int ziffer4= 13;
 
-int count=0;
+int minuten=0;
+int sekunden=0;
 int countto=0;
 
 const byte numbers[10] = {
@@ -21,6 +22,20 @@ const byte numbers[10] = {
   0b10110000, // 7
   0b10111111, // 8
   0b10111011  // 9
+};
+
+
+const byte numberswithp[10] = {
+  0b11111110, // 0
+  0b01110000, // 1
+  0b11101101, // 2
+  0b11111001, // 3
+  0b01110011, // 4
+  0b11011011, // 5
+  0b11011111, // 6
+  0b11110000, // 7
+  0b11111111, // 8
+  0b11111011  // 9
 };
 
 void setup()
@@ -44,17 +59,21 @@ void setup()
 
 void loop(){
 
-  timmer(count);
+  timmer(sekunden,minuten);
 
   countto +=4;
   if(countto>=1000){
-    count +=1;
+    sekunden +=1;
     countto = countto%1000;
+  }
+  if(sekunden>=60){
+    minuten++;
+    sekunden = sekunden %60;
   }
 
 }
 
-void timmer(int i_time){
+void timmer(int i_time,int i_minute){
 
   int tmp1=0;
   int tmp2=i_time;
@@ -81,12 +100,13 @@ void timmer(int i_time){
   delay(1);
   digitalWrite(ziffer3,HIGH);
 
+  tmp2 =i_minute;
     tmp1= tmp2 %10;
   tmp2 = tmp2/10;
 
   digitalWrite(ziffer2,LOW);
   digitalWrite(PIN_STORE, LOW);
-  shiftOut(PIN_DATA, PIN_SHIFT, LSBFIRST, numbers[tmp1]);
+  shiftOut(PIN_DATA, PIN_SHIFT, LSBFIRST, numberswithp[tmp1]);
   digitalWrite(PIN_STORE, HIGH);
   delay(1);
   digitalWrite(ziffer2,HIGH);
